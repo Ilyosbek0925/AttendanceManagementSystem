a0ac65655e05dac360f3c109ea3e02b242f603d8
@xakimovilyosbek22 <xakimovilyosbek22@gmail.com>
2025-09-12 21:23:48 +0500

    first commit

---
+++ src/main/java/school/entity/BaseEntity.java
@@ -0,0 +1,27 @@
+package school.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@MappedSuperclass
+@Getter
+@Setter
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    UUID uuid;
+    @CreationTimestamp
+    LocalDateTime createdAt;
+    @UpdateTimestamp
+    LocalDateTime updatedAt;
+
+    
+
+}
