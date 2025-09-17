352d0120776ed6825bd6ae7fc79a7f1bbb5022fe
asadbek <asadbekabdumamatov26@gmail.com>
2025-09-17 22:02:13 +0500

    yangi push

--- src/main/java/school/entity/Lesson.java
+++ src/main/java/school/entity/Lesson.java
@@ -1,6 +1,9 @@
 package school.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.*;
 
 @Entity
@@ -10,4 +13,17 @@ import lombok.*;
 @Setter
 @Builder(toBuilder = true)
 public class Lesson extends BaseEntity{
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(length = 500)
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "teacher_id")
+    private Teacher teacher;
+
+    @ManyToOne
+    @JoinColumn(name = "class_id")
+    private ClassEntity classEntity;
 }
