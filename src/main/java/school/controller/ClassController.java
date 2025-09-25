90a0d354f7d51edb9a08f358e5c7d1360d6c6b81
suhrob <suhrobravshanjanov@gmail.com>
2025-09-25 11:53:08 +0500

    suxrobjon

--- src/main/java/school/controller/ClassController.java
+++ src/main/java/school/controller/ClassController.java
@@ -1,4 +1,47 @@
 package school.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import school.dto.requestDto.ClassRequestDto;
+import school.dto.responseDto.ClassResponseDto;
+import school.service.ClassService;
+
+import java.net.URI;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/class")
+@RequiredArgsConstructor
 public class ClassController {
+
+    private final ClassService classService;`
+
+    @PostMapping("/add")
+    public ResponseEntity<ClassResponseDto> create(@RequestBody ClassRequestDto dto) {
+        ClassResponseDto created = classService.create(dto);
+        return ResponseEntity.created(URI.create("/api/classes/" + created.getUuid())).body(created);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<ClassResponseDto>> getAll() {
+        return ResponseEntity.ok(classService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClassResponseDto> getById(@PathVariable UUID id) {
+        return ResponseEntity.ok(classService.getById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ClassResponseDto> update(@PathVariable UUID id, @RequestBody ClassRequestDto dto) {
+        return ResponseEntity.ok(classService.update(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id) {
+        classService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/school/repository/ClassRepository.java
+++ src/main/java/school/repository/ClassRepository.java
@@ -1,4 +1,14 @@
 package school.repository;
 
-public interface ClassRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import school.entity.ClassEntity;
+
+import java.util.UUID;
+
+@Repository
+public interface ClassRepository extends JpaRepository<ClassEntity, UUID> {
+
+
+
 }
