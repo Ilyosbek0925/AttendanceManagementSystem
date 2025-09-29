5b23fc08a2d6bfb570f4cd3649208769dd35f2da
Doniyor <doniyorakramov11@gmail.com>
2025-09-29 17:24:49 +0500

    notification change boldi

--- src/main/java/school/entity/Notification.java
+++ src/main/java/school/entity/Notification.java
@@ -6,21 +6,19 @@ import lombok.*;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
-
-public class Notification extends BaseEntity{
+@Data
+public class Notification extends UserEntity{
 
     @Column(name="sender_name",nullable = false)
     private String senderName;
-
-
-
-    @Column(nullable = false)
+    @Column(name = "receiver_name",nullable = false)
+    private String recieverName;
+    @Column(name = "title",nullable = false)
     private String title;
-    @Column(nullable = false, columnDefinition = "TEXT")
+    @Column(name = "message",nullable = false, columnDefinition = "TEXT")
     private String message;
-    private boolean seen;
+    @Column(name = "is_read",nullable = false)
+    private boolean isRead;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id",nullable = false)
     private UserEntity user;
